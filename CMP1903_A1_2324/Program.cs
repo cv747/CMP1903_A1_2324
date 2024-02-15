6c5da6c5e442f3afcf1f828170f19eb2dba74514
Carter <144601471+cv747@users.noreply.github.com>
2024-02-15 16:46:40 +0000

    added statistics generation

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CMP1903_A1_2324
 {
@@ -13,20 +15,58 @@ namespace CMP1903_A1_2324
         public static void Main()
         {
             Game game = new Game();
-            (int total, int[] rolls) = game.RollDie();
 
-            foreach (int roll in rolls)
+            List<int> allRolls = new List<int>();
+            List<int> totals = new List<int>();
+
+            do
             {
-                Console.WriteLine("Rolled a " + roll);
-            }
+                (int total, int[] rolls) = game.RollDie();
 
-            Console.WriteLine("The total was " + total);
+                totals.Add(total);
 
+                foreach (int roll in rolls)
+                {
+                    Console.WriteLine("Rolled a " + roll + ".");
+                    allRolls.Add(roll);
+                }
+                Console.WriteLine("The total was " + total + ".");
+
+                Console.Write("\nWould you like to roll again? (press q for no) ");
+            } while (Console.ReadKey().Key != ConsoleKey.Q);
+
+            GenerateStats(allRolls, totals);
 
             Testing testing = new Testing();
             testing.TestDie();  // Test: Ensures Die class operates correctly.
             testing.TestArrayEqual();   // Test: Ensure array equality checking method works.
             testing.TestGame();  // Test: Ensure the game class works.
+        }
+
+        /// <summary>
+        /// Generates and prints out the statistics of all the rolls.
+        /// </summary>
+        public static void GenerateStats(List<int> rolls, List<int> totals)
+        {
+            Dictionary<int, int> rollCounter = new Dictionary<int, int>();
+            rolls.ForEach(roll =>
+            {
+                if (!rollCounter.ContainsKey(roll))
+                    rollCounter.Add(roll, 0);
+
+                rollCounter[roll]++;
+            });
+
+            float averageRoll = (float)rolls.Sum() / rolls.Count;
+            float averageTotal = (float)totals.Sum() / totals.Count;
+
+            Console.WriteLine("The average of the totals was " + Math.Round(averageTotal, 2) + ".");
+            foreach (KeyValuePair<int, int> entry in rollCounter)
+            {
+                Console.WriteLine("The number " + entry.Key + " occured " + entry.Value + " times.");
+            }
+            Console.WriteLine("The average of roll was " + Math.Round(averageRoll, 2) + ".");
+
         }
     }
 }
